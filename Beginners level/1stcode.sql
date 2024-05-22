2c428b3b93dc780d2ad48e6ed456dcb7160e3db1
C programming <154667994+Basabjeet-Deb@users.noreply.github.com>
2024-05-22 09:48:00 +0530

    Update 1stcode.sql

--- Beginners level/1stcode.sql
+++ Beginners level/1stcode.sql
@@ -1,4 +1,4 @@
-Find the titles of courses in the Comp. Sci. department that have 3 credits.
+*/Find the titles of courses in the Comp. Sci. department that have 3 credits.*/
 
 select title
 from course
